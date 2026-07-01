3053fa373b5cef747f7cc815f54ef459abcd6cca
alynguyxn <lyssbliss610@gmail.com>
2026-07-01 00:18:37 -0400

    optimizing problem 4

--- query.sql
+++ query.sql
@@ -1,18 +1,5 @@
 -- SentinelDB: Query
-
-EXPLAIN ANALYZE 
-SELECT * FROM network_logs WHERE attack_type = 'DDoS';
-
-DROP INDEX idx_severity_level;
-
-CREATE INDEX idx_severity_level ON network_logs(severity_level);
-
-DROP TABLE network_logs;
-
-
-
-
 EXPLAIN ANALYZE
-SELECT * FROM network_logs WHERE source_ip = '70.238.33.5';
+SELECT * FROM network_logs WHERE packet_length = '1500';
 
-CREATE INDEX idx_source_ip ON network_logs(source_ip);
+CREATE INDEX idx_packet_length ON network_logs(packet_length);
